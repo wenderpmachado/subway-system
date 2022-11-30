78f0f22dfcf0a3ea13d56c622d00302463c09aec
wenderpmachado <wenderpmachado@gmail.com>
2022-11-30 19:28:55 -0300

    feat(prisma): added generic service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { PrismaService } from './database/prisma.service';
+
+async function prismaBootstrap(app: INestApplication) {
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  await prismaBootstrap(app);
+
   await app.listen(3000);
 }
 bootstrap();
