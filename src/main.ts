68fbab546224cd73bfa3766be990320a7c42fb54
wenderpmachado <wenderpmachado@gmail.com>
2022-12-01 16:57:09 -0300

    docs(swagger): added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { PrismaService } from './database/prisma.service';
@@ -9,12 +10,27 @@ async function prismaBootstrap(app: INestApplication) {
   await prismaService.enableShutdownHooks(app);
 }
 
+function swaggerBootstrap(app) {
+  const config = new DocumentBuilder()
+    .setTitle('Subway System API')
+    .setDescription('The Subway System api')
+    .setVersion('0.1.8')
+    .addTag('route')
+    .addTag('train-line')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
   await prismaBootstrap(app);
+
+  swaggerBootstrap(app);
 
   await app.listen(3000);
 }
