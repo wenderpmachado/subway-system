4f36c223704c6910b4e59acee482f223de745fb6
wenderpmachado <wenderpmachado@gmail.com>
2022-11-30 21:06:56 -0300

    feat(train-line): created module + added on root

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrainLineModule } from './train-line/train-line.module';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [TrainLineModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
